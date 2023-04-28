45f347872f6d61e6215b8c209c7a40b65b743e7e
dominhtttb17 <dominhtttb17@gmail.com>
2023-04-29 00:14:38 +0700

    add crocss

--- style.js
+++ style.js
@@ -3,13 +3,13 @@ const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
 
-if(bar) {
+if (bar) {
     bar.addEventListener('click', () => {
         nav.classList.add('active')
     })
 }
 
-if(close) {
+if (close) {
     close.addEventListener('click', () => {
         nav.classList.remove('active')
     })
@@ -28,7 +28,7 @@ currentProductSizes.forEach((size, index) => {
             size.style.color = "black";
         });
         size.style.backgroundColor = "black";
-            size.style.color = "white";
+        size.style.color = "white";
     });
 });
 
@@ -40,47 +40,94 @@ let carts = document.querySelectorAll('.add');
 
 // Khai báo sản phẩm 
 
-let products = [
-    {
+let products = [{
         name: 'Women Crocs Baya Platform Sandals',
+        tag: 'women crocs baya platform sandals',
         price: 55,
-        size: ["39", "40", "41", "42"],
-        inCart:0
+        inCart: 0
     },
     {
         name: 'Women Crocs Baya Platform',
+        tag: 'women crocs baya platform',
         price: 75,
-        size: ["39", "40", "41", "42"],
-        inCart:0
+        inCart: 0
     },
     {
         name: 'Women Crocs Baya Platform Buggy',
+        tag: 'women crocs baya platform buggy',
         price: 65,
-        size: ["39", "40", "41", "42"],
-        inCart:0
+        inCart: 0
     },
 ]
 
 
-for (let i=0; i < carts.length; i++) {
+for (let i = 0; i < carts.length; i++) {
     carts[i].addEventListener('click', () => {
-        cartNumbers();
+        cartNumbers(products[i]);
+        totalCost(products[i])
     });
 }
 
-function cartNumbers() {
+function onLoadCartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers');
 
-    
+    if (productNumbers) {
+        document.querySelector('.cart span').textContent = productNumbers;
+    }
+}
+
+function cartNumbers(product) {
+
+
     let productNumbers = localStorage.getItem('cartNumbers');
 
     productNumbers = parseInt(productNumbers);
 
-    if( productNumbers ) {
+    if (productNumbers) {
         localStorage.setItem('cartNumbers', productNumbers + 1);
-        document.querySelector('cart span').textContent = productNumbers + 1;
-    }else {
+        document.querySelector('.cart span').textContent = productNumbers + 1;
+    } else {
         localStorage.setItem('cartNumbers', 1);
-        document.querySelector('cart span').textContent = 1;
+        document.querySelector('.cart span').textContent = 1;
+    }
+
+    setItems(product);
+}
+
+function setItems() {
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+
+    if (cartItems != null) {
+
+        if (cartItems[product.tag] == undefined) {
+            cartItems = {
+                ...cartItems,
+                [product.tag]: product
+            }
+        }
+
+        cartItems[product.tag].inCart += 1;
+    } else {
+        product.inCart = 1;
+        cartItems = {
+            [product.tag]: product
+        }
+    }
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+}
+
+function totalCost(product) {
+    let cartCost = localStorage.getItem('totalCost');
+    console.log("My cartCost is", cartCost);
+    console.log(typeof cartCost);
+
+    if (cartCost != null) {
+        localStorage.setItem('totalCost', cartCost + product.price);
+    } else {
+        localStorage.setItem('totalCost', product.price)
     }
 
 }
+
+onLoadCartNumbers();
